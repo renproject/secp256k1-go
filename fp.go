63dba3e2251032e91c7e1a2272677489fb69a9dc
ross <ross.pure@gmail.com>
2019-05-16 15:21:27 +1000

    initial implementaiton of wrapper struct

---
+++ fp.go
@@ -0,0 +1,69 @@
+package secp256k1
+
+/*
+#include <stdlib.h>
+
+#include "c-field/field_impl.h"
+#include "c-field/field_5x52.h"
+#include "c-field/field_5x52_impl.h"
+*/
+import "C"
+import (
+	"math/big"
+	"unsafe"
+)
+
+type Fp struct {
+	inner C.struct_secp256k1_fe
+}
+
+func NewFp(a uint64) Fp {
+	inner := C.struct_secp256k1_fe{}
+	C.secp256k1_fe_set_int(&inner, C.ulong(a))
+	return Fp{inner}
+}
+
+func (r *Fp) Set(a *Fp) {
+	r.inner = a.inner
+}
+
+func (r *Fp) SetBytes(b []byte) {
+	b32 := make([]byte, 32)
+	copy(b32[32-len(b):], b)
+	cBytes := (*C.uchar)(C.CBytes(b32))
+	defer C.free(unsafe.Pointer(cBytes))
+	C.secp256k1_fe_set_b32(&r.inner, cBytes)
+}
+
+func (r *Fp) Bytes() []byte {
+	cBytes := C.malloc(C.sizeof_char * 32)
+	defer C.free(unsafe.Pointer(cBytes))
+	C.secp256k1_fe_get_b32((*C.uchar)(cBytes), &r.inner)
+	b := C.GoBytes(cBytes, 32)
+	return b
+}
+
+func (r *Fp) BigInt() *big.Int {
+	return big.NewInt(0).SetBytes(r.Bytes())
+}
+
+func (r *Fp) Add(a, b *Fp) {
+	r.Set(a)
+	C.secp256k1_fe_add(&r.inner, &b.inner)
+	C.secp256k1_fe_normalize_var(&r.inner)
+}
+
+func (r *Fp) Neg(a *Fp) {
+	C.secp256k1_fe_negate(&r.inner, &a.inner, 1)
+	C.secp256k1_fe_normalize_var(&r.inner)
+}
+
+func (r *Fp) Mul(a, b *Fp) {
+	C.secp256k1_fe_mul(&r.inner, &a.inner, &b.inner)
+	C.secp256k1_fe_normalize_var(&r.inner)
+}
+
+func (r *Fp) Inv(a *Fp) {
+	C.secp256k1_fe_inv_var(&r.inner, &a.inner)
+	C.secp256k1_fe_normalize_var(&r.inner)
+}
