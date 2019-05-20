4c58b085af0bfa1d72a8c4f183fd9ac4e24b0112
ross <ross.pure@gmail.com>
2019-05-20 14:26:29 +1000

    add trials to tests

--- secp256k1_test.go
+++ secp256k1_test.go
@@ -9,8 +9,9 @@ import (
 	"github.com/republicprotocol/secp256k1-go"
 )
 
-var _ = Describe("Wrapped Fp elements", func() {
+var _ = Describe("Wrapped field elements", func() {
 	P, _ := big.NewInt(0).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16)
+	TRIALS := 10000
 
 	randomBig256 := func() *big.Int {
 		b := make([]byte, 32)
@@ -18,52 +19,59 @@ var _ = Describe("Wrapped Fp elements", func() {
 		return big.NewInt(0).SetBytes(b)
 	}
 
-	Context("When doing arithmetic", func() {
+	Context("When doing arithmetic in Fp", func() {
 		It("Should add correctly", func() {
-			x, y := randomBig256(), randomBig256()
-			sum := big.NewInt(0).Add(x, y)
-			sum.Mod(sum, P)
-			a := secp256k1.NewFp(0)
-			b := secp256k1.NewFp(0)
-			c := secp256k1.NewFp(0)
-			a.SetBytes(x.Bytes())
-			b.SetBytes(y.Bytes())
-			c.Add(&a, &b)
-			Expect(c.BigInt().Cmp(sum)).To(Equal(0))
+			for i := 0; i < TRIALS; i++ {
+				x, y := randomBig256(), randomBig256()
+				sum := big.NewInt(0).Add(x, y)
+				sum.Mod(sum, P)
+				a := secp256k1.NewFp(0)
+				b := secp256k1.NewFp(0)
+				c := secp256k1.NewFp(0)
+				a.SetBytes(x.Bytes())
+				b.SetBytes(y.Bytes())
+				c.Add(&a, &b)
+				Expect(c.BigInt().Cmp(sum)).To(Equal(0))
+			}
 		})
 
 		It("Should negate correctly", func() {
-			x := randomBig256()
-			neg := big.NewInt(0).Sub(P, x)
-			a := secp256k1.NewFp(0)
-			b := secp256k1.NewFp(0)
-			a.SetBytes(x.Bytes())
-			b.Neg(&a)
-			Expect(b.BigInt().Cmp(neg)).To(Equal(0))
+			for i := 0; i < TRIALS; i++ {
+				x := randomBig256()
+				neg := big.NewInt(0).Sub(P, x)
+				a := secp256k1.NewFp(0)
+				b := secp256k1.NewFp(0)
+				a.SetBytes(x.Bytes())
+				b.Neg(&a)
+				Expect(b.BigInt().Cmp(neg)).To(Equal(0))
+			}
 		})
 
 		It("Should multiply correctly", func() {
-			x, y := randomBig256(), randomBig256()
-			prod := big.NewInt(0).Mul(x, y)
-			prod.Mod(prod, P)
-			a := secp256k1.NewFp(0)
-			b := secp256k1.NewFp(0)
-			c := secp256k1.NewFp(0)
-			a.SetBytes(x.Bytes())
-			b.SetBytes(y.Bytes())
-			c.Mul(&a, &b)
-			Expect(c.BigInt().Cmp(prod)).To(Equal(0))
+			for i := 0; i < TRIALS; i++ {
+				x, y := randomBig256(), randomBig256()
+				prod := big.NewInt(0).Mul(x, y)
+				prod.Mod(prod, P)
+				a := secp256k1.NewFp(0)
+				b := secp256k1.NewFp(0)
+				c := secp256k1.NewFp(0)
+				a.SetBytes(x.Bytes())
+				b.SetBytes(y.Bytes())
+				c.Mul(&a, &b)
+				Expect(c.BigInt().Cmp(prod)).To(Equal(0))
+			}
 		})
 
 		It("Should invert correctly", func() {
-			x := randomBig256()
-			inv := big.NewInt(0).ModInverse(x, P)
-			a := secp256k1.NewFp(0)
-			b := secp256k1.NewFp(0)
-			a.SetBytes(x.Bytes())
-			b.Inv(&a)
-			Expect(b.BigInt().Cmp(inv)).To(Equal(0))
+			for i := 0; i < TRIALS; i++ {
+				x := randomBig256()
+				inv := big.NewInt(0).ModInverse(x, P)
+				a := secp256k1.NewFp(0)
+				b := secp256k1.NewFp(0)
+				a.SetBytes(x.Bytes())
+				b.Inv(&a)
+				Expect(b.BigInt().Cmp(inv)).To(Equal(0))
+			}
 		})
-
 	})
 })
